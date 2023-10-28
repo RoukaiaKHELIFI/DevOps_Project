434c582dae299302a9789cab19efbf3825f93319
oussama jmaa <67621280+oussamaJMAA@users.noreply.github.com>
2023-10-28 23:52:51 +0100

    stock

---
+++ DevOps_Project-main/DevOps_Project/src/test/java/tn/esprit/devops_project/services/StockServiceImplTest.java
@@ -0,0 +1,53 @@
+package tn.esprit.devops_project.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import tn.esprit.devops_project.entities.Stock;
+import tn.esprit.devops_project.repositories.StockRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@ExtendWith(MockitoExtension.class)
+class StockServiceImplTest {
+
+    Stock s = new Stock(1L,"stock1",null);
+
+    List<Stock> stocks = new ArrayList<>(){
+        {
+            add(new Stock(2L,"stock2",null));
+            add(new Stock(3L,"stock3",null));
+        }
+    };
+    @Mock
+    StockRepository stockRepository ;
+    @InjectMocks
+    StockServiceImpl stockServiceImpl ;
+    @Test
+    void addStock() {
+        Mockito.when(stockRepository.save(Mockito.any(Stock.class))).thenReturn(s);
+        Stock s1 = stockServiceImpl.addStock(s);
+        Assertions.assertNotNull(s1);
+    }
+
+    @Test
+    void retrieveStock() {
+        Mockito.when(stockRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(s));
+        Stock s1 = stockServiceImpl.retrieveStock(1L);
+        Assertions.assertNotNull(s1);
+        System.out.println(s1.getTitle());
+    }
+
+    @Test
+    void retrieveAllStock() {
+        Mockito.when(stockRepository.findAll()).thenReturn(stocks);
+        List<Stock> s1 = stockServiceImpl.retrieveAllStock();
+        Assertions.assertEquals(2,s1.size());
+    }
+}
