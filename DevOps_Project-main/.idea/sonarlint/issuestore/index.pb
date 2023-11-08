2267894d72f17d26cc3d0e903e50b24d44322fa6
Aicha0 <aicha.benmessaoud@esprit.tn>
2023-11-08 11:09:02 +0100

    ADD TEST

---
+++ DevOps_Project-main/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+‡
+WDevOps_Project/src/main/java/tn/esprit/devops_project/services/OperatorServiceImpl.java,e\3\e3927e1cdc30377a22cd163946915ce5cc72a943
+‹
+[DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java,1\a\1ad20d741a46dfef072427280e8118340852e32f
---
+++ DevOps_Project-main/DevOps_Project/src/test/java/tn/esprit/devops_project/services/OperatorServiceImplTest.java
@@ -0,0 +1,97 @@
+package tn.esprit.devops_project.services;
+
+
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.devops_project.entities.Operator;
+import tn.esprit.devops_project.services.Iservices.IOperatorService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import static org.mockito.Mockito.when;
+
+import tn.esprit.devops_project.repositories.OperatorRepository;
+
+
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+class OperatorServiceImplTest {
+    @Mock
+    OperatorRepository operatorRepository;
+    @InjectMocks
+    OperatorServiceImpl operatorService;
+
+
+
+
+
+    Operator operator = new Operator(1L, "John", "Doe", "password123", null);
+    List<Operator> operators = new ArrayList<>(){
+        {
+            add(new Operator(1L, "John", "Doe", "password123", null));
+            add(new Operator(2L, "Jane", "Doe", "password456", null));
+        }
+    };
+
+
+
+
+
+    //Mockito
+    @Test
+    void retrieveOperator() {
+        Mockito.when(operatorRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(operator));
+        Operator operator1 = operatorService.retrieveOperator(1L);
+        Assertions.assertNotNull(operator1);
+        System.out.println(operator1.getLname());
+    }
+    //Mockito
+    @Test
+    void deleteOperator() {
+        Mockito.doNothing().when(operatorRepository).deleteById(Mockito.anyLong());
+
+        operatorService.deleteOperator(1L);
+
+        Mockito.verify(operatorRepository, Mockito.times(1)).deleteById(1L);
+
+    }
+    //JUnit
+    @Test
+    void updateOperator() {
+        List<Operator> operatorList = new ArrayList<>();
+        operatorList.add(operatorService.updateOperator(operator));
+        Assertions.assertNotNull(operatorList);
+    }
+    //Mockito
+    @Test
+    void retrieveAllOperators() {
+
+        when(operatorService.retrieveAllOperators()).thenReturn(operators);
+        List<Operator> operatorList = operatorService.retrieveAllOperators();
+        assertEquals(2, operatorList.size());
+        System.out.println( operatorList);
+        System.out.println("Retreive All Operators Completed..........");
+    }
+
+    // JUnit
+    @Test
+    void addOperator() {
+        List<Operator> test = new ArrayList<>();
+        test.add(operatorService.addOperator(operator));
+        Assertions.assertNotNull(test);
+    }
+}
+
+
+
+
